fd13552e7a8c6bdae05f59609dad7d7fe8284c15
Michael Cabelin <ncabelin@gmail.com>
2017-01-08 16:58:57 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# geocoder
-ExpressJS Google Maps API Geocoder server
+# Google Maps API Geocode server
+
+## Synopsis
+
+This server app returns the geographic coordinates ( longitude, latitude ) in JSON format
+using NodeJS, Express and the Google Maps API
+
+## Routes
+The home route ('/') will return the following instructions if hosted in Heroku :
+'Please put zipcode onto URL to get geolocation coordinates : e.g. https://googlygps.herokuapp.com/90016'
+
+## Installation
+1. Install NodeJS and ExpressJS 'npm install express --save'
+2. Make sure to include a module named 'key.js' with your Google Maps API key in it on the home directory :
+
+```
+module.exports = '&key=INSERT_KEY_HERE';
+```
+3. Run the app, 'node server.js'
+
+## Contributors
+
+Neptune Michael Cabelin
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var request = require('request');
+var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+var key = require('./key');
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://psych-unit-locator.firebaseapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+app.get('/', function(req, res) {
+  res.end('Please put zipcode onto URL to get geolocation coordinates : e.g. https://googlygps.herokuapp.com/90016');
+});
+
+
+app.get('/:zipcode', function(req, res) {
+  var zip = req.params.zipcode;
+  var urlComplete = url + zip + key;
+  request(urlComplete, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      if (data.results[0]) {
+       var loc = data.results[0].geometry.location;
+       res.json(loc);
+       return;
+      }
+    } else {
+      console.log(error);
+    }
+  });
+});
+
+
+app.listen(process.env.PORT || 8080, function() {
+  console.log('Started server...');
+});
